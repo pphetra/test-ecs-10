d8249e089cb52cd91540fa83f78414edc633846e
pphetra <pphetra@gmail.com>
2021-03-25 16:35:00 +0700

    change text

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! pok pok pok';
+    return 'Hello World! pok pok bunn bunn';
   }
 }
